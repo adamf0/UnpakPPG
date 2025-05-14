707d220c6e7cc421d8f50228f7cf6ae80d51424f
!Github@251423! <studiocode52@gmail.com>
2025-05-14 12:43:44 +0700

    update frontend

--- resources/js/Persistance/API.jsx
+++ resources/js/Persistance/API.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8000";
+const baseUrl = "https://lapordiri-ppg.unpak.ac.id";
 
 const apiProduction = axios.create({
     baseURL: baseUrl,
