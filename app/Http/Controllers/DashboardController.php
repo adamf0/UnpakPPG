0cf0ca68e6e587cbc390641e50791ee7001e4e49
!Github@251423! <studiocode52@gmail.com>
2025-05-17 14:14:38 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LaporDiri;
+use App\Models\mahasiswa;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function dashboardPage(){
-        return Inertia::render("Admin/Dashboard",[]);
+        $totalRegistrasi = mahasiswa::count();
+        
+        $lengkap = LaporDiri::where("status","done")->count();
+        $tidakLengkap = LaporDiri::whereNot("status","done")->count();
+        $tidakTerdaftar = $totalRegistrasi - $lengkap - $tidakLengkap;
+
+        return Inertia::render("Admin/Dashboard",["lengkap"=>$lengkap,"tidakLengkap"=>$tidakLengkap,"tidakTerdaftar"=>$tidakTerdaftar]);
     }
 }
