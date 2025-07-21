d33acf26f3aab3b0138926964d93436765940c14
adamf <adamilkom00@gmail.com>
2025-07-21 11:15:16 +0700

    add meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LAPOR DIRI - PPG</title>
+    <meta name="robots" content="noindex, nofollow">
+
+    <!-- Meta Deskripsi -->
+    <meta name="description" content="Sistem Pelaporan Data PPG Universitas Pakuan">
+
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="Lapor Diri, PPG, Unpak, Universitas Pakuan">
+
+    <!-- Cegah penyisipan iframe dari domain lain (klikjacking protection) -->
+    <meta http-equiv="X-Frame-Options" content="DENY">
+
+    <!-- Cegah XSS di browser lama -->
+    <meta http-equiv="X-XSS-Protection" content="1; mode=block">
+
+    <!-- Cegah penebakan jenis konten -->
+    <meta http-equiv="X-Content-Type-Options" content="nosniff">
+
     @viteReactRefresh
     @vite(['resources/css/app.css','resources/js/app.jsx'])
 </head>
