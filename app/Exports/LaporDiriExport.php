df4a2bde2cf4693bdcf72fb223fd677baebbdc55
adamf <adamilkom00@gmail.com>
2025-07-26 18:30:40 +0700

    update

--- app/Exports/LaporDiriExport.php
+++ app/Exports/LaporDiriExport.php
@@ -26,19 +26,11 @@ class LaporDiriExport implements FromCollection, WithHeadings, WithTitle
         $version = env("Version",null);
 
         if ($this->filter_status == '!done') {
-            // dd(
-            //     DB::table("all_record")
-            //         ->select("all_record.*","mahasiswa.nama","mahasiswa.bidangStudi","mahasiswa.jenjangSekolah", "mahasiswa.provinsi",DB::raw("mahasiswa.noHP as nomorHp"))
-            //         ->join("mahasiswa", "all_record.nomorUKG", "mahasiswa.nomorUKG")
-            //         ->whereNull("status")
-            //         ->where('all_record.version',$version)
-            //         ->toRawSql()
-            // );
             return DB::table("all_record")
                     ->select("all_record.*","mahasiswa.nama","mahasiswa.bidangStudi","mahasiswa.jenjangSekolah", "mahasiswa.provinsi",DB::raw("mahasiswa.noHP as nomorHp"))
                     ->join("mahasiswa", "all_record.nomorUKG", "mahasiswa.nomorUKG")
                     ->whereNull("status")
-                    ->where('all_record.version',$version)
+                    ->where('mahasiswa.version',$version)
                     ->get()
                     ->map(function($mahasiswa) use($isDev){
                         return [
