cdfbfb2232cd6089606841be28a1d60e83f22203
adamf0 <adamilkom00@gmail.com>
2026-09-25 09:42:38 +0700

    update banner

--- resources/js/Pages/PendaftaranPage.jsx
+++ resources/js/Pages/PendaftaranPage.jsx
@@ -729,7 +729,7 @@ function PendaftaranPage({ activeMenu }) {
             </div>
             {errNomorUKG.map(err => <p className="text-red-500 text-sm">{err}</p>)}
 
-            <img src="https://lapordiri-ppg.unpak.ac.id/banner_ppg.jpeg" alt="banner" className='mt-8'/>
+            <img src="https://lapordiri-ppg.unpak.ac.id/banner_ppg.png" alt="banner" className='mt-8'/>
         </>
     }
     function BiodataPage(){
